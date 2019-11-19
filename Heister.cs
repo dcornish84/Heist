8c1f53a907a6409c341e69e41e7e5b38f1d9f2c6
David Cornish <davidcornish@Davids-MacBook-Pro.local>
2019-11-19 09:57:53 -0600

    updated

---
+++ Heister.cs
@@ -0,0 +1,12 @@
+
+
+namespace HeistExercise
+{
+    public class Heister
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public decimal CourageFactor { get; set; }
+    }
+
+}
